d2128aa32a898f9d22f05f7062d9f00dce614e83
Sevinj Allahverdiyeva <sevindzallphaerdieva@gmail.com>
2024-10-18 13:04:12 +0400

    add cmakelists.txt

---
+++ build/CMakeFiles/Euler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Euler.dir/euler.cpp.o"
+  "CMakeFiles/Euler.dir/euler.cpp.o.d"
+  "libEuler.pdb"
+  "libEuler.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Euler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build1/CMakeFiles/calculate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculate.dir/euler_mod.cxx.o"
+  "CMakeFiles/calculate.dir/euler_mod.cxx.o.d"
+  "calculate"
+  "calculate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
